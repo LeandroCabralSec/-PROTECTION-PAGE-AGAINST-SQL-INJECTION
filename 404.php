94e0cafaab568fecff83b3f891e00d240a55e52a
Leandro Cabral <68610108+LeandroCabralSec@users.noreply.github.com>
2020-07-21 20:16:44 -0300

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,3 +1,12 @@
+Os ataques de injeção ocorrem quando as entradas criadas com códigos maliciosos são enviadas por um invasor, fazendo com que um aplicativo execute uma ação não 
+intencional. A injeção de SQL é um dos tipos mais comuns de ataque na Internet. Com essa página no seu tema, qualquer código colocado na URL do seu site, retornará 
+para HOME, impedindo de alterações na sua URL.
+
+Injection attacks occur when entries created with malicious code are sent by an attacker, causing an application to perform an unintended action. 
+SQL injection is one of the most common types of attacks on the Internet. With this page in your theme, any code placed in your website's URL will 
+return to HOME, preventing changes to your URL.
+
+
 /** PÁGINA DE PROTEÇÃO CONTRA SQL INJECTION, CRIADA NO TEMA RAÍZ */
 / ** PROTECTION PAGE AGAINST SQL INJECTION, CREATED IN THE ROOT THEME * /
 
